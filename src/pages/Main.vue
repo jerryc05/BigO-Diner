d60e54a794f7ea6153b602ece041cf57e66aac0b
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-04-22 00:58:59 -0400

    upd logo and title

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -10,8 +10,8 @@
   <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
       <a href="/" class="flex items-center">
-        <img src="/logo.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        <img src="/logo.png" class="mr-3 h-6 sm:h-9" />
+        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">BigO Diner</span>
       </a>
       <div class="flex md:order-2">
         <div class="hidden relative mr-3 md:mr-0 md:block">
