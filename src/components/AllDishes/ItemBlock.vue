eb823e45338cc2af1ab3f4f68f706569d4bdf86f
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-05-13 19:49:58 -0400

    tracking adobe ai file in lfs, add price icon

--- src/components/AllDishes/ItemBlock.vue
+++ src/components/AllDishes/ItemBlock.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
 import bingbing from '@/assets/bingbing.png'
+import fish from '@/assets/fish.svg'
+import catFood from '@/assets/catFood.svg'
+import catFoodCan from '@/assets/catFoodCan.svg'
 import { darkBgBigOColor } from '@/utils/colors'
 import type { Item } from '@/menu/menu_types'
+import PriceImg from './PriceImg.vue'
 
 const props = defineProps<{ item: Item }>()
+
+const getRandPrice = () => Math.floor(Math.random() * 200)
+
+let x = getRandPrice()
+const price: [number, number, number] = [Math.floor(x / 100), 0, 0]
+x %= 100
+price[1] = Math.floor(x / 10)
+x %= 10
+price[2] = x
 </script>
 
 
@@ -14,9 +27,27 @@ const props = defineProps<{ item: Item }>()
 <template>
   <div class="h-24 p-2 m-2 rounded flex" :class="[darkBgBigOColor]">
     <img :src='bingbing' class="h-full p-1 aspect-square">
-    <span class='ml-2 flex flex-col'>
+    <span class='ml-2 w-full flex flex-col justify-center'>
+      <!-- Title -->
       <b>{{ props.item.cnName }} · {{ props.item.enName }}</b>
+
+      <!-- Category & Chefs -->
       <span>{{ props.item.category.cnName }} | {{ props.item.chefs.join(' · ') }}</span>
+
+      <!-- Price -->
+      <span class="self-end">
+        <span class="inline-flex" v-if="price[0] > 0">
+          {{ price[0] }}
+          <PriceImg :src="catFood" />
+        </span>
+        <span class="inline-flex" v-if="price[0] > 0 || price[1] > 0">
+          {{ price[1] }}
+          <PriceImg :src="catFoodCan" />
+        </span>
+        <span class="inline-flex">{{ price[2] }}
+          <PriceImg :src="fish" />
+        </span>
+      </span>
     </span>
   </div>
 </template>
---
+++ src/components/AllDishes/PriceImg.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{ src: string }>()
+</script>
+
+
+
+
+
+
+ <template>
+  <img :src="props.src" class="ml-1 mr-4 h-6 max-h-full aspect-square inline-block dark:invert" />
+</template>
