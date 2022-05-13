32d51580914c3b8c6bc56e54423f744ba63fb21d
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-05-13 01:12:49 -0400

    show dishes with cate.

--- src/components/AllDishes/ItemBlock.vue
+++ src/components/AllDishes/ItemBlock.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import bingbing from '@/assets/bingbing.png'
 import { darkBgBigOColor } from '@/utils/colors'
 import type { Item } from '@/menu/menu_types'
 
@@ -11,6 +12,11 @@ const props = defineProps<{ item: Item }>()
 
 
 <template>
-  <button class="w-32 h-10 rounded m-1 bg-gray-300" :class="[darkBgBigOColor]"
-    @click="(e) => { (e.target as HTMLButtonElement).innerText = '妙啊' }">{{ props.item.cnName }}</button>
+  <div class="h-24 p-2 m-2 rounded flex" :class="[darkBgBigOColor]">
+    <img :src='bingbing' class="h-full p-1 aspect-square">
+    <span class='ml-2 flex flex-col'>
+      <b>{{ props.item.cnName }} · {{ props.item.enName }}</b>
+      <span>{{ props.item.category.cnName }} | {{ props.item.chefs.join(' · ') }}</span>
+    </span>
+  </div>
 </template>
--- src/components/Category/ItemBlock.vue
+++ src/components/Category/ItemBlock.vue
@@ -11,5 +11,5 @@ const props = defineProps<{ text: string }>()
 
 <template>
   <button class="w-32 h-10 rounded m-1 bg-gray-300" :class="[darkBgBigOColor]"
-    @click="(e) => { (e.target as HTMLButtonElement).innerText = '妙啊' }">{{ props.text }}</button>
+    @click="e => { (e.target as HTMLButtonElement).innerText = '妙啊' }">{{ props.text }}</button>
 </template>
