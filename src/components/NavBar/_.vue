fb5e285b2504b0d5934ba9681235bc9ee0ab3529
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-06-16 17:38:27 -0400

    fix navbar not full width

--- src/components/NavBar/_.vue
+++ src/components/NavBar/_.vue
@@ -16,7 +16,7 @@ const navBarHeight = 'h-16'
 
 
 <template>
-  <nav class="px-2 fixed top-0 rounded-b-lg shadow-md bg-gray-300 z-10 grid grid-cols-7 place-content-center"
+  <nav class="px-2 w-full fixed top-0 rounded-b-lg shadow-md bg-gray-300 z-10 grid grid-cols-7 place-content-center"
     :class="[darkBgBigOColor, navBarHeight]">
     <!-- Title & icon -->
     <span class="col-start-1 sm:col-start-2 sm:col-span-5 sm:flex sm:place-content-center">
