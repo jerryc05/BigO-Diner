80030e5f115192a054fe28149e38c6f1d32e750c
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-06-18 20:23:17 -0400

    upd margin

--- src/App.vue
+++ src/App.vue
@@ -17,19 +17,17 @@ const store = useStore()
   <div class="w-full min-w-80 min-h-screen dark:bg-black dark:text-white" :class='[textMtColor]'>
     <NavBar />
 
-    <div class="px-4">
-      <BodyPart />
+    <BodyPart />
 
-      <img :src="store.isDark ? 'https://i.ibb.co/rs7THLg/We-Chat-Image-20220423044752.jpg' : bingbing"
-        class="h-36 object-contain ml-auto mr-auto" />
+    <img :src="store.isDark ? 'https://i.ibb.co/rs7THLg/We-Chat-Image-20220423044752.jpg' : bingbing"
+      class="h-36 object-contain ml-auto mr-auto" />
 
-      <div>{{ store.isDark ? '大橘：老吴~~~啊~吴~吃~~麻麻香' : '大饼：嗷呜~~猫呜~~母猫~~猫粮呜' }}！</div>
+    <div>{{ store.isDark ? '大橘：老吴~~~啊~吴~吃~~麻麻香' : '大饼：嗷呜~~猫呜~~母猫~~猫粮呜' }}！</div>
 
-      <button type="button" class="text-white rounded relative left-1/2 -translate-x-1/2 p-2"
-        :class="[darkBgBigOColor, bgMtColor]" @click="store.toggleLightDarkMode">Change
-        to
-        {{ store.isDark ? 'Light' : 'Dark' }} mode</button>
-    </div>
+    <button type="button" class="text-white rounded relative left-1/2 -translate-x-1/2 p-2"
+      :class="[darkBgBigOColor, bgMtColor]" @click="store.toggleLightDarkMode">Change
+      to
+      {{ store.isDark ? 'Light' : 'Dark' }} mode</button>
   </div>
 </template>
 
--- src/components/body/_.vue
+++ src/components/body/_.vue
@@ -9,8 +9,8 @@ import AllDishes from './AllDishes/_.vue'
 
 
 <template>
-  <div class="sm:mx-5">
-      <Category />
-      <AllDishes />
+  <div class="mx-10 sm:mx-15 md:mx-20 lg:mx-25 xl:mx-30 2xl:mx-35">
+    <Category />
+    <AllDishes />
   </div>
 </template>
