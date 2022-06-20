ec42f02d1535223f35fb5f60db2d6b55f3c74766
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-06-20 04:24:49 -0400

    upd vite.config.ts eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'vue',
     '@typescript-eslint'
   ],
+  ignorePatterns: ['**/*.d.ts', 'node_modules/**', 'dist/**'],
   rules: {
     'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
@@ -42,12 +43,18 @@ module.exports = {
     'vue/v-on-function-call': ['error', 'never'],
 
     'array-element-newline': ['error', 'consistent'],
+    'arrow-parens': ['error', 'as-needed'],
     'comma-dangle': ['error', 'only-multiline'],
     'dot-location': ['error', 'property'],
     'func-style': ['error', 'declaration'],
     'function-call-argument-newline': ['error', 'never'],
-    'no-extra-parens': ['error', 'all', {'nestedBinaryExpressions': false}],
+    'function-paren-newline': ['error', 'consistent'],
+    'no-extra-parens': [
+      'error', 'all', {'nestedBinaryExpressions': false,
+        'enforceForArrowConditionals': false }
+    ],
     'padded-blocks': ['error', 'never'],
+    'sort-imports': ['error', { 'ignoreCase': true }],
 
     'capitalized-comments': 'off',
     'id-length': 'off',
@@ -59,6 +66,7 @@ module.exports = {
     'max-params': 'off',
     'multiline-comment-style': 'off',
     'multiline-ternary': 'off',
+    'no-console': 'off',
     'no-inline-comments': 'off',
     'no-magic-numbers': 'off',
     'no-ternary': 'off',
@@ -67,5 +75,4 @@ module.exports = {
     'sort-keys': 'off',
     'sort-vars': 'off',
   },
-  ignorePatterns: ['**/*.d.ts', 'node_modules/**', 'dist/**'],
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,55 @@
-import { constants } from 'fs'
-import { access, open, readdir, readFile, stat, writeFile } from 'fs/promises'
 import * as path from 'path'
+
+import { access, open, readdir, readFile, stat, writeFile } from 'fs/promises'
+import { defineConfig, HtmlTagDescriptor, normalizePath, Plugin } from 'vite'
+
 import { brotliCompress } from 'zlib'
-
-import vue from '@vitejs/plugin-vue'
-import { defineConfig, normalizePath, Plugin, HtmlTagDescriptor } from 'vite'
+import { constants } from 'fs'
 import { createHtmlPlugin } from 'vite-plugin-html'
-import { viteExternalsPlugin } from 'vite-plugin-externals'
 import removeConsole from 'vite-plugin-remove-console'
-import WindiCSS from 'vite-plugin-windicss'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
+import vue from '@vitejs/plugin-vue'
+import windiCSS from 'vite-plugin-windicss'
 
 
-// https://vitejs.dev/config/
-const target = 'esnext'
-const jsdelivr = 'https://cdn.jsdelivr.net'
-const tags: HtmlTagDescriptor[] = [{
-  injectTo: 'head-prepend',
-  tag: 'meta',
-  attrs: {
-    'http-equiv': 'Content-Security-Policy',
-    // [*-elem] doesn't work in Safari/iOS, fvck Safari
-    content: `default-src 'self';script-src 'self' ${jsdelivr};style-src 'self' 'unsafe-inline' ${jsdelivr};img-src 'self' data: https://\*.ibb.co https://\*.licdn.com`,
-  },
-},
-{
-  injectTo: 'head-prepend',
-  tag: 'meta',
-  attrs: {
-    'http-equiv': 'X-Content-Type-Options',
-    content: 'nosniff',
-  },
-},
-{
-  injectTo: 'head-prepend',
-  tag: 'meta',
-  attrs: {
-    name: 'viewport',
-    content: 'width=device-width,initial-scale=1.0',
-  },
-},
-{
-  injectTo: 'head-prepend',
-  tag: 'meta',
-  attrs: {
-    charset: 'UTF-8',
-  },
-},
-]
-const extPlugs: Plugin[] = []
+const target = 'esnext',
+  jsdelivr = 'https://cdn.jsdelivr.net',
+  tags: HtmlTagDescriptor[] = [
+    {
+      injectTo: 'head-prepend',
+      tag: 'meta',
+      attrs: {
+        'http-equiv': 'Content-Security-Policy',
+        // [*-elem] doesn't work in Safari/iOS, fvck Safari
+        content: `default-src 'self';script-src 'self' ${jsdelivr};style-src 'self' 'unsafe-inline' ${jsdelivr};img-src 'self' data: https://*.ibb.co https://*.licdn.com`,
+      },
+    },
+    {
+      injectTo: 'head-prepend',
+      tag: 'meta',
+      attrs: {
+        'http-equiv': 'X-Content-Type-Options',
+        content: 'nosniff',
+      },
+    },
+    {
+      injectTo: 'head-prepend',
+      tag: 'meta',
+      attrs: {
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1.0',
+      },
+    },
+    {
+      injectTo: 'head-prepend',
+      tag: 'meta',
+      attrs: {
+        charset: 'UTF-8',
+      },
+    },
+  ],
+  extPlugs: Plugin[] = []
+
 if (process.env.NODE_ENV === 'production') {
   tags.push(
     {
@@ -57,16 +59,14 @@ if (process.env.NODE_ENV === 'production') {
         defer: true,
         src: 'https://cdn.jsdelivr.net/npm/vue@3/dist/vue.runtime.global.prod.js',
       },
-    },
-    {
+    }, {
       injectTo: 'head',
       tag: 'script',
       attrs: {
         defer: true,
         src: 'https://cdn.jsdelivr.net/npm/vue-demi',
       },
-    },
-    {
+    }, {
       injectTo: 'head',
       tag: 'script',
       attrs: {
@@ -88,8 +88,35 @@ if (process.env.NODE_ENV === 'production') {
     pinia: 'Pinia',
     // '@vueuse/core': 'VueUse'
   }))
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument
   extPlugs.push(removeConsole())
 }
+
+function buildPostProcessor (fn: (p: string) => Promise<void> | void): Plugin {
+  let outRoot = ''
+  async function iterDir (dir: string) {
+    await readdir(dir).then(xs => Promise.all(xs.map(
+      async x_ => {
+        const x = path.resolve(dir, x_)
+        await stat(x).then(async s => (s.isDirectory() ? iterDir(x) : fn(x)))
+      }
+    )))
+  }
+  return {
+    name: buildPostProcessor.name,
+    enforce: 'post',
+    apply: 'build',
+    configResolved (conf) {
+      outRoot = normalizePath(path.resolve(conf.root, conf.build.outDir))
+    },
+    async closeBundle () {
+      await access(outRoot, constants.F_OK).then(() => iterDir(outRoot))
+    }
+  }
+}
+
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -102,35 +129,35 @@ export default defineConfig({
           template: 'index.html',
           injectOptions: {
             data: {
-              htmlLang: '"en-US"',  // '"zh-cmn-Hans"',
+              htmlLang: '"en-US"', // '"zh-cmn-Hans"',
             },
             tags,
           },
         }
       ]
     }),
-    WindiCSS(),
+    windiCSS(),
     ...extPlugs,
-    MyPostProcessorOnBuild(async p => {
-      if (/\.(\w?js|css)$/.test(p)) {
+    buildPostProcessor(async p => {
+      if ((/\.(?:\w?js|css)$/u).test(p)) {
         let content = (await readFile(p)).toString()
-        const commentRegex = /\/\*[\s\S]*?\*\//g
+        const commentRegex = /\/\*[\s\S]*?\*\//ug
         if (commentRegex.test(content)) {
           content = content.replace(commentRegex, '')
           await open(p, 'w').then(async f => f.write(content))
         }
       }
     }),
-    MyPostProcessorOnBuild(async p => {
-      p = p.split(path.sep).join(path.posix.sep)
-      if (/\.+\w?(js|css|html)$/.test(p)) {
-        const orig = await readFile(p)
-        const compressed: Buffer = await new Promise((acc, rej) =>
-          brotliCompress(orig.buffer, (e, b) => e ? rej(e) : acc(b))
-        )
+    buildPostProcessor(async p_ => {
+      const p = p_.split(path.sep).join(path.posix.sep)
+      if ((/\.+\w?(?:js|css|html)$/u).test(p)) {
+        const orig = await readFile(p),
+          compressed: Buffer = await new Promise((acc, rej) => {
+            brotliCompress(orig.buffer, (e, b) => (e ? rej(e) : acc(b)))
+          }),
+          origSz = orig.length,
+          newSz = compressed.length
         await writeFile(p, compressed)
-        const origSz = orig.length
-        const newSz = compressed.length
         console.log(`${p}\n\t${origSz} \t-> ${newSz} bytes \t${(newSz / origSz).toFixed(4)}x`)
       } else {
         console.log(`${p} \tskipped ...`)
@@ -138,12 +165,12 @@ export default defineConfig({
     })
   ],
   build: {
-    reportCompressedSize: false,  // improve speed
+    reportCompressedSize: false, // improve speed
     target
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),  // check tsconfig.json => paths
+      '@': path.resolve(__dirname, './src'), // check tsconfig.json => paths
     },
   },
   // base: ''  # Default: '/'
@@ -169,30 +196,3 @@ export default defineConfig({
     // }
   }
 })
-
-
-
-
-
-function MyPostProcessorOnBuild(fn: (p: string) => Promise<void> | void): Plugin {
-  let outRoot: string
-  async function iterDir(dir: string) {
-    await readdir(dir).then(xs => xs.forEach(async x => {
-      x = path.resolve(dir, x)
-      await stat(x).then(async s => {
-        if (!s.isDirectory()) await fn(x)
-        else await iterDir(x)
-      })
-    }
-    ))
-  }
-  return {
-    name: MyPostProcessorOnBuild.name,
-    enforce: "post",
-    apply: "build",
-    configResolved(conf) {
-      outRoot = normalizePath(path.resolve(conf.root, conf.build.outDir))
-    },
-    async closeBundle() { access(outRoot, constants.F_OK).then(_ => iterDir(outRoot)) }
-  }
-}
