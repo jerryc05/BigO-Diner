e3c75e82fdf51e927677ca68e5c5af342ac43498
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-06-21 01:39:14 -0400

    bug fix

--- src/components/body/Category/ItemBlock.vue
+++ src/components/body/Category/ItemBlock.vue
@@ -3,12 +3,17 @@ import { darkBgBigOColor } from '@/utils/colors'
 import type { Item } from '@/menu/menu_types'
 import { useStore } from '@/stores'
 
+
 const store = useStore(),
   dc = store.disabledCategories,
-  props = defineProps<{ item: Item }>()
+  props = defineProps<{ item: Item }>(),
+  /* eslint-disable vue/no-setup-props-destructure */
+  t = props.item.constructor.name,
+  cateName = props.item.category.cnName
+  /* eslint-enable vue/no-setup-props-destructure */
+
 
 function toggleCategory () {
-  const t = props.item.constructor.name
   if (dc.has(t)) {
     dc.delete(t)
     console.log('rm', t)
@@ -31,6 +36,6 @@ function toggleCategory () {
     :class="[darkBgBigOColor,{ 'line-through': dc.has(t) }]"
     @click="toggleCategory"
   >
-    {{ (!dc.has(t)) ? props.item.category.cnName : `已过滤${props.item.category.cnName}` }}
+    {{ (!dc.has(t)) ?cateName : `已过滤${cateName}` }}
   </button>
 </template>
