259b4ac84037e8cba0e9c741a3cfcc2effb38d88
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-11-19 04:15:10 -0500

    other tweaks

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
+import { ErrorBoundary } from "solid-js"
 
 import './index.css'
 import App from './App'
 
 const mount = document.createElement('div')
 document.body.insertBefore(mount, document.body.firstChild)
-render(() => <App />, mount)
+render(() =>
+  <ErrorBoundary fallback={(err, reset) =>
+    <div style={{ 'text-align': 'center' }}>
+      <div>{err}</div>
+      <button onClick={reset}>Reset</button>
+    </div>}>
+    <App />
+  </ErrorBoundary>, mount)
