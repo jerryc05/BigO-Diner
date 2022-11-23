d66b8be62a1a282bf559f87f0dfbb92fd452f8c3
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-11-23 03:37:20 -0500

    add normalize; working on navbar

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,14 @@
 /*
-pnpm i -D postcss postcss-at-rules-variables postcss-csso postcss-preset-env postcss-variable-compress
+pnpm i -D modern-normalize postcss postcss-at-rules-variables postcss-csso postcss-preset-env postcss-variable-compress
 */
 
 /* eslint-disable unicorn/prefer-module, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment */
 module.exports = {
   plugins: [
     require('postcss-at-rules-variables'),
+    require('postcss-preset-env')({ stage: 0 }),
+
+
     ...process.env.NODE_ENV === 'production'
       ? [
         require('postcss-csso')({
@@ -15,6 +18,5 @@ module.exports = {
         require('postcss-variable-compress'),
       ]
       : [],
-    require('postcss-preset-env')({ stage: 0 }),
   ]
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
 import './index.css'
+import 'modern-normalize/modern-normalize.css'
 
 import { ErrorBoundary } from 'solid-js'
 import { render } from 'solid-js/web'
