cbc127654fe8c0b4cf2fc87a45bf2c66848f727a
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-11-23 02:29:41 -0500

    postcss variable

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 /*
-pnpm i -D postcss postcss-at-rules-variables postcss-csso postcss-preset-env
+pnpm i -D postcss postcss-at-rules-variables postcss-csso postcss-preset-env postcss-variable-compress
 */
 
 /* eslint-disable unicorn/prefer-module, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment */
@@ -11,7 +11,8 @@ module.exports = {
         require('postcss-csso')({
           comments: false,
           forceMediaMerge: true,
-        })
+        }),
+        require('postcss-variable-compress'),
       ]
       : [],
     require('postcss-preset-env')({ stage: 0 }),
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,38 +2,35 @@
 import bigO from '@/assets/bigO.png'
 import bingbing from '@/assets/bingbing.png'
 import cartIcon from '@/assets/cart.svg'
-import { cart, isDark, setShowCart } from '@/states'
+import { cart, isDark, setShowCart, showCart } from '@/states'
 // import { darkBgBigOColor} from '@/utils/colors'
 import { isDev } from '@/utils/constants'
 import ScrSize from '@/utils/ScrSize'
 
-import css from './index.module.css'
-
+import css from './index.module.pcss'
 
 export default () => <>
   <nav>
+    {/* Dark btn */}
 
-    {/* Back btn */}
-    {/* <button type="button" class="invisible">Back</button> */}
+    {/* Cart */}
+    <button class={css.cartBtn} type='button'
+      disabled={showCart()}
+      onClick={() => setShowCart(true)}>
+      <img src={cartIcon} />
+      { isDev ? <ScrSize/> : null }
+      { isDev ? ' - ' : null }
+      { cart.size }
+    </button>
 
-    {/* Dark btn & Cart & Username & Avatar */}
-    <span class='flex justify-end'>
-      {/* Dark btn */}
-      {/* Cart */}
-      <button type='button' class='px-3 mr-2 my-1 flex flex-center
-      rounded-full shadow whitespace-nowrap bg-white text-xl text-black' onClick={() => setShowCart(true)}>
-        <img class='h-6 pr-2' src={cartIcon} />
-        { isDev ? <ScrSize/> : null }
-        { cart.size }
-      </button>
-      {/* Username */}
-      <b class='hidden sm:inline-flex flex-center text-lg mx-2 whitespace-nowrap'>jerryc05</b>
-      {/* Avatar */}
-      <button type='button' class='h-12 flex flex-center rounded-full shadow aspect-square
-      focus:ring-gray-300 dark:focus:ring-gray-600 bg-white'>
-        <img class='h-5/7 aspect-square' src={isDark() ? bigO : bingbing}/>
-      </button>
-    </span>
+    {/* Username */}
+    <b class='hidden sm:inline-flex flex-center text-lg mx-2 whitespace-nowrap'>jerryc05</b>
+
+    {/* Avatar */}
+    <button type='button'
+      class={css.avatarBtn}>
+      <img src={isDark() ? bigO : bingbing}/>
+    </button>
 
   </nav>
 
