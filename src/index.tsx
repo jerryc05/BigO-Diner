38535da90324e302ed62d22f131150bb5c3a1fdb
Jerry "Ziyan" Chen <jerryc443@gmail.com>
2022-12-11 02:48:33 -0500

    rm err

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,10 @@
 import './index.scss'
 import 'modern-normalize/modern-normalize.css'
 
-import { ErrorBoundary } from 'solid-js'
 import { render } from 'solid-js/web'
 
 import App from './App'
 
 const mount = document.createElement('div')
 document.body.insertBefore(mount, document.body.firstChild)
-render(() => (
-  <ErrorBoundary fallback={(err, reset) => (
-    <div style={{ 'text-align': 'center' }}>
-      <div>{err}</div>
-      <div>{JSON.stringify(err)}</div>
-      <button type='button' onClick={reset}>Reset</button>
-    </div>)}
-  >
-    <App />
-  </ErrorBoundary>), mount)
+render(() => <App />, mount)
