393d62a58cc722e693f036568808ea900f326f66
Jerry "Ziyan" Chen <jerryc443@gmail.com>
2022-12-12 01:19:26 -0500

    scrsize outside cart

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,40 +3,43 @@ import bigO from '@/assets/bigO.png'
 import bingbing from '@/assets/bingbing.png'
 import cartIcon from '@/assets/cart.svg'
 import { cart, isDark, setShowCart, showCart } from '@/states'
+import ScrSize from '@/utils/ScrSize'
 // import { darkBgBigOColor} from '@/utils/colors'
 import { isDev } from '@/utils/constants'
-import ScrSize from '@/utils/ScrSize'
 
 import css from './index.module.scss'
 
 export default () => (
   <>
     <nav>
-      {/* Dark btn */}
+      {/* Dark Mode Btn */}
+
+      {/* ScrSize */}
+      {isDev ? <ScrSize /> : null}
 
       {/* Cart */}
-      <button class={css.cartBtn} type='button'
-              disabled={showCart()}
-              onClick={() => setShowCart(true)}
+      <button
+        class={css.cartBtn}
+        type='button'
+        disabled={showCart()}
+        onClick={() => setShowCart(true)}
       >
         <img src={cartIcon} />
-        { isDev ? <ScrSize/> : null }
-        { isDev ? ' - ' : null }
-        { cart.size }
+        {cart.size}
       </button>
 
       {/* Username */}
-      <b class='hidden sm:inline-flex flex-center text-lg mx-2 whitespace-nowrap'>jerryc05</b>
+      <b class='hidden sm:inline-flex flex-center text-lg mx-2 whitespace-nowrap'>
+        jerryc05
+      </b>
 
       {/* Avatar */}
-      <button type='button'
-              class={css.avatarBtn}
-      >
+      <button type='button' class={css.avatarBtn}>
         <img src={isDark() ? bigO : bingbing} />
       </button>
-
     </nav>
 
     {/* Offset */}
     <div class={css.navBarOffsetAboveBody} />
-  </>)
+  </>
+)
