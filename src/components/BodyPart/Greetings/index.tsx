5e2ade006f345ef24393d0d0d973311e91e5e921
Jerry "Ziyan" Chen <jerryc443@gmail.com>
2022-12-12 01:13:32 -0500

    greeting

--- src/components/BodyPart/Greetings/index.tsx
+++ src/components/BodyPart/Greetings/index.tsx
@@ -1,11 +1,14 @@
 import { isDark } from '@/states'
 
-export default ()=> (<>
- <div class="mb-3 text-3xl">
-    Hi,
-    <b>jerryc05!</b>
-  </div>
-  <div class="mb-2 text-md uppercase">
-    Welcome to {isDark() ? 'BigO' : 'MilkTea' } Diner!
-  </div>
-</>)
+import css from './index.module.scss'
+
+export default () => (
+  <>
+    <div class={css.hi}>
+      Hi, <b>jerryc05!</b>
+    </div>
+    <div class={css.welcome}>
+      Welcome to {isDark() ? 'BigO' : 'MilkTea'} Diner!
+    </div>
+  </>
+)
