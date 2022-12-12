16b958a8872831edc12981eac803486349bff2f5
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-12-12 18:17:52 -0500

    color

--- src/utils/colors.ts
+++
@@ -1,8 +0,0 @@
-export const bgBigOColor = 'bg-[#ab641b]'
-export const darkBgBigOColor = 'dark:bg-[#ab641b]'
-export const textBigOColor = 'text-[#ab641b]'
-export const darkTextBigOColor = 'dark:text-[#ab641b]'
-export const bgMtColor = 'bg-[#3e2d14]'
-export const darkBgMtColor = 'dark:bg-[#3e2d14]'
-export const textMtColor = 'text-[#3e2d14]'
-export const darkTextMtColor = 'dark:text-[#3e2d14]'
