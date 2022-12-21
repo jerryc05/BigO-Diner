fbd0da1e4b98659c3ae4a9fb4e2f2bf0044e286e
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2022-12-21 14:40:25 -0500

    add safari bs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import { detect } from 'detect-browser'
+import { createSignal } from 'solid-js'
+
 import css from './App.module.scss'
 import BodyPart from './components/BodyPart'
 import CartDetail from './components/CartDetail'
 import NavBar from './components/NavBar'
 
-const App = () => (
-  <div class={css.app}>
-    <CartDetail />
-    <NavBar />
-    <BodyPart />
-  </div>
-)
+const App = () => {
+  const [acknowledgeSafari, setAcknowledgeSafari] = createSignal(false)
+
+  return (
+    <div class={css.app}>
+      {acknowledgeSafari() || detect()?.name !== 'safari' ? (
+        <>
+          <CartDetail />
+          <NavBar />
+          <BodyPart />
+        </>
+      ) : (
+        <button
+          onClick={() => {
+            setAcknowledgeSafari(true)
+          }}
+        >
+          Safari is bullshit!
+        </button>
+      )}
+    </div>
+  )
+}
 
 export default App
