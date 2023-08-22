69986874e88176d26d0820d8f66be588ae6074c3
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-08-21 23:25:11 -0400

    u

--- src/components/BodyPart/Greetings/index.tsx
+++ src/components/BodyPart/Greetings/index.tsx
@@ -5,7 +5,10 @@ import css from './index.module.scss'
 export default () => (
   <>
     <div class={css.greetings}>
-      Hi, <b>{user()?.username ?? 'Please login'}!</b>
+      Hi,
+      <b>
+        {user.loading ? 'Please wait...' : user()?.username ?? 'Please login'}!
+      </b>
     </div>
     <div class={css.welcome}>
       Welcome to {isDark() ? 'BigO' : 'MilkTea'} Diner!
