9bb2e9a482b0e87851c3d6f3a48a91b351f6d628
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-08-28 20:09:03 -0400

    no public needed

--- deploy_dist.sh
+++ deploy_dist.sh
@@ -3,16 +3,16 @@ set -exuo pipefail
 
 rm -rf dist || true
 npm run build
-rm -rf ../dist ../public || true
+rm -rf ../dist  || true
 mv dist public ..
 cp vercel.json ..
 
 git switch dist
 git reset --hard $(git rh)
 
-rm -rf dist public || true
-mv ../dist ../public ../vercel.json .
-git add dist public vercel.json
+rm -rf dist || true
+mv ../dist ../vercel.json .
+git add dist vercel.json
 
 git status
 git commit -m 'dist'
