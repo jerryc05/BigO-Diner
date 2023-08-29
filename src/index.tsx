5d2f246fd8d7aafbde208945e8ffc086a75f2ac8
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-08-29 18:51:58 -0400

    bugfix reload

--- src/index.tsx
+++ src/index.tsx
@@ -6,20 +6,26 @@ import App from './App'
 import './index.css'
 import { SW_PATH } from './utils/constants'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register(SW_PATH)
-    .then(reg => {
-      reg.update()
-      location.replace(
-        // Force all requests are cached,
-        // especially those send before sw is installed,
-        // otherwise offline cache won't be complete
-        location.href
-      )
-    })
-    .catch(err => console.error(err))
-}
+navigator.serviceWorker
+  ?.register(SW_PATH)
+  .then(reg => {
+    // reg.update()
+    reg.onupdatefound = () => {
+      const newWorker = reg.installing
+      if (newWorker)
+        newWorker.onstatechange = () => {
+          if (newWorker.state == 'activated') {
+            location.replace(
+              // Force all requests are cached,
+              // especially those send before sw is installed,
+              // otherwise offline cache won't be complete
+              location.href,
+            )
+          }
+        }
+    }
+  })
+  .catch(err => console.error(err))
 
 const mount = document.createElement('div')
 document.body.insertBefore(mount, document.body.firstChild)
