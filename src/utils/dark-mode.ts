b39f48588cd016085da995e1b3e9ae507540e488
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2023-08-28 20:07:49 -0400

    rm favicon change based on dark

--- src/utils/dark-mode.ts
+++ src/utils/dark-mode.ts
@@ -4,26 +4,26 @@ import bingbing from '@/assets/bingbing.png'
 const bodyClasses = document.body.classList
 let icon: HTMLLinkElement | null = null
 
-function favicon() {
-  if (icon === null) {
-    const link = document.head.querySelector('link[rel~="icon"]')
-    if (link === null) {
-      icon = document.createElement('link')
-      icon.rel = 'icon'
-      document.head.append(icon)
-    } else icon = link as HTMLLinkElement
-  }
-  return icon
-}
+// function favicon() {
+//   if (icon === null) {
+//     const link = document.head.querySelector('link[rel~="icon"]')
+//     if (link === null) {
+//       icon = document.createElement('link')
+//       icon.rel = 'icon'
+//       document.head.append(icon)
+//     } else icon = link as HTMLLinkElement
+//   }
+//   return icon
+// }
 
 function setDark() {
-  favicon().href = bigO
+  // favicon().href = bigO
   document.title = 'BigO Diner'
   bodyClasses.add('dark')
 }
 
 function setLight() {
-  favicon().href = bingbing
+  // favicon().href = bingbing
   document.title = 'MilkTea Diner'
   bodyClasses.remove('dark')
 }
